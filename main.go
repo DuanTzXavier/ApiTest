de6e879c7d1b161e6a139ef7897cff9d29345f2b
tzduan <tzduan@Ctrip.com>
2017-10-17 12:54:50 +0800

    add server and json response

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"ApiTest/dbStructure"
 	"net/http"
 	"log"
+	"strings"
+	"encoding/json"
 )
 
 func main() {
@@ -20,9 +22,33 @@ func main() {
 }
 func TestServer(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Inside HelloServer handler")
-
-	fmt.Fprintf(writer, "Hello,"+request.URL.Path[1:] + " header: ")
+	var t dbStructure.UserBaseInfo
+	t.User_ID = "asa"
+	t.User_Name = "name"
+	t.User_Avatar = "avator"
+	t.User_Status = 1
+	result := dbhelper.QueryData("users_base_info", nil, nil, &t)
+	fmt.Println(result)
+	var paramSlice []string
+	for _, param := range *result {
+		b, err := json.Marshal(param)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+		paramSlice = append(paramSlice, string(b))
+	}
+	aa := strings.Join(paramSlice, "")
+	fmt.Fprintf(writer, "Hello,"+request.URL.Path[1:] + "test: " + aa)
 	fmt.Println(request.Header)
+}
+
+func getName(params ...interface{}) {
+	var paramSlice []string
+	for _, param := range params {
+		paramSlice = append(paramSlice, param.(string))
+	}
+	aa := strings.Join(paramSlice, "_") // Join 方法第2个参数是 string 而不是 rune
+	fmt.Println(aa)
 }
 
 func testDB() {
